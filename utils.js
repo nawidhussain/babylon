45d04cbc8a736f2fb8dfcbb55254043190ed3a07
nawidhussain <afnawidafnawid@gmail.com>
2025-01-12 00:46:45 +0530

    Delete utils.js

--- utils.js
+++
@@ -1,9 +0,0 @@
-
-export function retrieveUserData() {
-    const params = new URLSearchParams(window.location.search);
-    return {
-        userId: params.get('user_id') || null,
-        username: params.get('username') || null,
-        firstName: params.get('first_name') || "Guest",
-    };
-}
